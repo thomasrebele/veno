fe51e41684340227f3af44cf98b34b3a78f0b7b0
thomasrebele <thomas.rebele@mv-buchdorf.de>
2012-04-30 18:45:31 +0200

    test

--- veno-java/src/Main.java
+++ veno-java/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		System.out.println("gujigula");
 		System.out.println("guliguliguli");
 		System.out.println("lalalagugugu du bist ein papalein");
+		System.out.println("gagagagaga");
 		
 	}
 
